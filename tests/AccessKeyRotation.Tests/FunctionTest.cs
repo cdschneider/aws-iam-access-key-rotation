eb73f135810d1c7d2b3cf36fd29e43293a7dd661
Christian Schneider <schneider.christiandavid@gmail.com>
2023-01-27 12:28:30 -0600

    starting on unit-testing new function

--- tests/AccessKeyRotation.Tests/FunctionTest.cs
+++ tests/AccessKeyRotation.Tests/FunctionTest.cs
@@ -1,9 +1,31 @@
-using Xunit;
-using Amazon.Lambda.Core;
+using AccessKeyRotation.Models;
 using Amazon.Lambda.TestUtilities;
+using Moq.AutoMock;
+using Xunit;
 
 namespace AccessKeyRotation.Tests;
 
 public class FunctionTest
 {
+    private readonly Function _classUnderTest;
+    
+    public FunctionTest()
+    {
+        var mocker = new AutoMocker();
+        _classUnderTest = mocker.CreateInstance<Function>();
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidFunctionHandlerInputs))]
+    public Task TestFunctionHandler_WhenInputIncludesNullFields_ThenArgumentExceptionIsThrown(AccessKeyRotationRequest input)
+        => Assert.ThrowsAsync<ArgumentException>(() => 
+            _classUnderTest.FunctionHandler(input, new TestLambdaContext()));
+
+    public static IEnumerable<object[]> InvalidFunctionHandlerInputs =>
+        new List<object[]>
+        {
+            new object[] { (null as AccessKeyRotationRequest)! },
+            new object[] { new AccessKeyRotationRequest { UserName = "username_456" } },
+            new object[] { new AccessKeyRotationRequest { AccessKeyId = "access_key123" } },
+        };
 }
