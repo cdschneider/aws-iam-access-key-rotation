9768126ac836d48a407d6161e73fb86c4d9a773d
Christian Schneider <schneider.christiandavid@gmail.com>
2023-03-06 23:27:59 -0600

    more removal

--- src/AccessKeyActions/Startup.cs
+++ src/AccessKeyActions/Startup.cs
@@ -1,6 +1,4 @@
 using AccessKeyActions.Configuration;
-using Amazon.DynamoDBv2;
-using Amazon.IdentityManagement;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
